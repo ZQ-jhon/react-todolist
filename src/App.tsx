7fc700fafc828385634f420f7d6c83b18df2e30e
zhangqiang <350037310@qq.com>
2019-06-16 21:11:15 +0800

    feat: :sparkles: adding react icon

--- src/App.tsx
+++ src/App.tsx
@@ -43,24 +43,27 @@ export default class App extends React.Component<Props<any>, {}> {
   }
   public render() {
     return (
-      <div className="App">
-        <ul>
-          <input type="text" value={this.state.inputValue} onKeyPress={this.keypress.bind(this)} onChange={this.handleChange} />
-          <button onClick={this.updateView}>添加</button>
-        </ul>
-        <ul>
-         {
-           this.state.list.map((item: string, index: number) => {
-             return (
-              <li key={index}>
-                <span>{item}</span> 
-                <button onClick={this.deleteItem.bind(this,index)}>删除</button>
-              </li>
-             );
-           })
-         }
-        </ul>
-      </div>
+     <div>
+        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"/>
+        <div className="App">
+          <ul>
+            <input type="text" value={this.state.inputValue} onKeyPress={this.keypress.bind(this)} onChange={this.handleChange} />
+            <button onClick={this.updateView}>添加</button>
+          </ul>
+          <ul>
+            {
+              this.state.list.map((item: string, index: number) => {
+                return (
+                <li key={index}>
+                  <span>{item}</span> 
+                  <button onClick={this.deleteItem.bind(this,index)}>删除</button>
+                </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+     </div>
     );
   }
 }
