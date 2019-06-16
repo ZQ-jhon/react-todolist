9b43b550913355026eec1a1d7076b9664155195b
zhangqiang <350037310@qq.com>
2019-06-16 20:54:48 +0800

    feat: :sparkles: build react

---
+++ build/precache-manifest.b7e3308765f16fad1920d2bb0bc894fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89da73340ac0bfe9b6c70ac5940a5bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44134612cf57cd4b6465",
+    "url": "/static/css/main.52c0ded6.chunk.css"
+  },
+  {
+    "revision": "1a20620b78c516895c8c",
+    "url": "/static/js/2.fe6d7557.chunk.js"
+  },
+  {
+    "revision": "44134612cf57cd4b6465",
+    "url": "/static/js/main.6ea0d9fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
